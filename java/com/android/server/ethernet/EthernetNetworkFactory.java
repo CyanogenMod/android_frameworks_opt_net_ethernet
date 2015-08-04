0e47a8aae62e79d80db150bdde3298a100ab2d03
Lorenzo Colitti <lorenzo@google.com>
2015-08-04 13:27:28 +0900

    Stop DHCP if it times out.
    This was not necessary in L, but it is necessary after
    https://partner-android-review.googlesource.com/#/c/205415/ .
    
    Bug: 22936203
    Change-Id: I24a96c33e5bb375f36217a1c2cec2076eb5ac8b4

--- java/com/android/server/ethernet/EthernetNetworkFactory.java
+++ java/com/android/server/ethernet/EthernetNetworkFactory.java
@@ -262,7 +262,7 @@ class EthernetNetworkFactory {
         // TODO: Handle DHCP renew.
         Thread dhcpThread = new Thread(new Runnable() {
             public void run() {
-                if (DBG) Log.i(TAG, "dhcpThread(+" + mIface + "): mNetworkInfo=" + mNetworkInfo);
+                if (DBG) Log.i(TAG, "dhcpThread(" + mIface + "): mNetworkInfo=" + mNetworkInfo);
                 LinkProperties linkProperties;
 
                 IpConfiguration config = mEthernetManager.getConfiguration();
@@ -287,6 +287,9 @@ class EthernetNetworkFactory {
                         // set our score lower than any network could go
                         // so we get dropped.
                         mFactory.setScoreFilter(-1);
+                        // If DHCP timed out (as opposed to failing), the DHCP client will still be
+                        // running, because in M we changed its timeout to infinite. Stop it now.
+                        NetworkUtils.stopDhcp(mIface);
                         return;
                     }
                     linkProperties = dhcpResults.toLinkProperties(mIface);
